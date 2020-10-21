7ce577377ab1efd4f1cb3dcd74fc0983c317f82a
Michael Piastucki <44167171+happymanju@users.noreply.github.com>
2020-10-21 11:54:23 +0900

    Initial Commit; Added state for AllPhotos
    Where does this go?!
    
    Co-Authored-By: Antonio De Rinaldis <44536723+dius00@users.noreply.github.com>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/styles.css";
+import Navbar from "../components/Navbar";
+import AllPhotos from "../components/AllPhotos";
 
 export default function App() {
+  // currentView usestate()
+
+  const [currentView, SetCurrentView] = useState("SinglePhoto");
+
   return (
-    <div className="app">
-      <h1>Hello World!</h1>
-    </div>
+    <>
+      <div className="app">
+        {/*
+        NAVBAR
+          NAVBAR > UPLOAD
+          
+        PHOTOS(currentviews)
+      */}
+
+        <h1>Hello World!</h1>
+        <Navbar SetCurrentView={SetCurrentView} />
+        <h3>{currentView}</h3>
+        {currentView === "AllPhotos" ? <AllPhotos /> : null}
+      </div>
+    </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 import "../styles/navbar.css";
 import _ from "lodash";
+import Upload from "./Upload";
 
-export default function Navbar() {
-  return <div className="navbar" />;
+export default function Navbar({ SetCurrentView }) {
+  return (
+    <>
+      <div className="navbar" onClick={() => SetCurrentView("AllPhotos")}>
+        Test Navigation Bar
+      </div>
+      />
+      <Upload />
+    </>
+  );
 }
